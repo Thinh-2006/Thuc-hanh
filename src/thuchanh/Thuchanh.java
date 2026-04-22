7cbfce8e2ea4ec9a26b83431d2fab7cdf8f161bf
daika <lamnguyentienthinh@gmail.com>
2026-04-22 07:47:19 +0700

    lan dau

---
+++ src/thuchanh/Thuchanh.java
@@ -0,0 +1,11 @@
+
+package thuchanh;
+
+public class Thuchanh {
+
+    public static void main(String[] args) {
+        JFThuchanh c = new JFThuchanh ();
+        c.show();
+    }
+    
+}
---
+++ src/thuchanh/thuchanhchunhat.java
@@ -0,0 +1,38 @@
+
+package thuchanh;
+
+public class thuchanhchunhat {
+    private double a;
+    private double b;
+    private double ketQua;
+
+    public thuchanhchunhat(double a, double b, double ketQua) {
+        this.a = a;
+        this.b = b;
+        this.ketQua = ketQua;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getKetQua() {
+        return ketQua;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public void setKetQua(double ketQua) {
+        this.ketQua = ketQua;
+    }
+}
